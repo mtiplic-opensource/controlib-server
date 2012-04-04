64e92ce547ecb2d9b7669462178cf299bcdb2bf3
Mogmi <mogmi95@gmail.com>
2012-04-04 14:52:09 +0200

    Added Qrcode generator and Connexion class

---
+++ src/main/java/com/epita/j2ee/controlibserver/connection/ConnectionManager.java
@@ -0,0 +1,89 @@
+package com.epita.j2ee.controlibserver.connection;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+/**
+ * This class contains methods to open a connection from the server on a network
+ * interface and to obtain the list of available interfaces
+ *
+ * @author Mickael "Mogmi" Bidon
+ */
+public class ConnectionManager
+{
+
+  /**
+   * The ServerSocket which will handle the connection
+   */
+  private ServerSocket server;
+
+  public ConnectionManager()
+  {
+  }
+
+  /**
+   * Open a connection on the network
+   *
+   * @param port The port on which the connection is opened
+   * @return
+   */
+  public boolean openConnection(int port) throws IOException
+  {
+    if (this.server != null)
+    {
+      return (false);
+    }
+    this.server = new ServerSocket(port);
+    return (true);
+  }
+
+  /**
+   * Close the connection
+   *
+   * @throws IOException
+   */
+  public void closeConnection() throws IOException
+  {
+    this.server.close();
+  }
+
+  /**
+   * Get the list of available network interfaces
+   *
+   * @return A map containing the name of the interface and the IP address
+   * linked to it
+   */
+  public HashMap<String, HashMap<String, String>> getInterfaces() throws SocketException
+  {
+    Enumeration e = NetworkInterface.getNetworkInterfaces();
+    HashMap<String, HashMap<String, String>> res = new HashMap<String, HashMap<String, String>>();
+
+    while (e.hasMoreElements())
+    {
+      NetworkInterface ni = (NetworkInterface) e.nextElement();
+      HashMap<String, String> hash = new HashMap<String, String>();
+
+      Enumeration e2 = ni.getInetAddresses();
+      while (e2.hasMoreElements())
+      {
+        InetAddress ip = (InetAddress) e2.nextElement();
+        if (ip instanceof Inet4Address)
+        {
+          hash.put("IPV4", ip.getHostAddress());
+        } else if (ip instanceof Inet6Address)
+        {
+          hash.put("IPV6", ip.getHostAddress());
+        }
+
+      }
+      res.put(ni.getName(), hash);
+    }
+    return res;
+  }
+
+  public ServerSocket getServer()
+  {
+    return server;
+  }
+}
---
+++ src/main/java/com/epita/j2ee/controlibserver/qrcode/QrcodeGenerator.java
@@ -0,0 +1,49 @@
+package com.epita.j2ee.controlibserver.qrcode;
+
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * This class contains static methods which generate QRcode used to init
+ * connection with the remote client
+ *
+ * @author Mickael "Mogmi" Bidon
+ */
+public class QrcodeGenerator
+{
+
+  public static BufferedImage generateQrcode(String ip, int port, int width, int height) throws WriterException
+  {
+    BitMatrix mtx;
+    String input = ip + "|" + String.valueOf(port);
+    QRCodeWriter writer = new QRCodeWriter();
+    mtx = writer.encode(input, BarcodeFormat.QR_CODE, width, height);
+    if (mtx != null)
+    {
+      BufferedImage image = MatrixToImageWriter.toBufferedImage(mtx);
+      return image;
+    }
+    return null;
+  }
+
+  public static void generateQrcodeToFile(File file, String format, String ip, int port, int width, int height) throws WriterException, IOException
+  {
+    BitMatrix mtx;
+    String input = ip + "|" + String.valueOf(port);
+    QRCodeWriter writer = new QRCodeWriter();
+    mtx = writer.encode(input, BarcodeFormat.QR_CODE, width, height);
+    if (mtx != null)
+    {
+      BufferedImage image = MatrixToImageWriter.toBufferedImage(mtx);
+      ImageIO.write(image, format, file);
+    }
+  }
+}
